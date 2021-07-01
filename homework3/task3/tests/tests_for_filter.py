d8498a56705c3d887d98a1f41f7c7d8d7f74435b
IvanPiankov <vanypyankov@gmail.com>
2021-07-01 23:18:11 +0300

    second_change

--- homework3/task3/tests/tests_for_filter.py
+++ homework3/task3/tests/tests_for_filter.py
@@ -3,7 +3,12 @@ import homework3.task3.Filter.filter as fil
 
 def test_of_sample_data():
     expected_result = [
-        {"is_dead": True, "kind": "parrot", "type": "bird", "name": "polly",}
+        {
+            "is_dead": True,
+            "kind": "parrot",
+            "type": "bird",
+            "name": "polly",
+        }
     ]
     actual_result = fil.make_filter(name="polly", type="bird").apply(fil.sample_data)
 
--- homework3/task4/tests/tests_arm_number.py
+++ homework3/task4/tests/tests_arm_number.py
@@ -3,7 +3,11 @@ from homework3.task4.arm_num.arm_num import is_armstrong
 
 
 @pytest.mark.parametrize(
-    ["integer", "expected_result"], [(-1, False), (10, False),],
+    ["integer", "expected_result"],
+    [
+        (-1, False),
+        (10, False),
+    ],
 )
 def test_check_false_result_is_arm(integer: int, expected_result: bool):
     result = is_armstrong(integer)
@@ -11,7 +15,12 @@ def test_check_false_result_is_arm(integer: int, expected_result: bool):
 
 
 @pytest.mark.parametrize(
-    ["integer", "expected_result"], [(153, True), (0, True), (1, True),],
+    ["integer", "expected_result"],
+    [
+        (153, True),
+        (0, True),
+        (1, True),
+    ],
 )
 def test_check_true_result_is_arm(integer: int, expected_result: bool):
     result = is_armstrong(integer)
