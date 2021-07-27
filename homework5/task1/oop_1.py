d23c90f9e379362cad2b0369260964b99fc8a038
IvanPiankov <vanypyankov@gmail.com>
2021-07-27 16:34:02 +0300

    Add_ docstrings_for_classes_and_method

--- homework5/task1/oop_1.py
+++ homework5/task1/oop_1.py
@@ -38,24 +38,36 @@ import datetime
 
 
 class Homework:
+    """
+     This class stores homework for student
+    """
     def __init__(self, text_of_task: str, day_number: int):
         self.text = text_of_task
         self.deadline = datetime.timedelta(day_number)
         self.created = datetime.datetime.now()
 
     def is_active(self):
-        if (self.deadline + self.created) > self.created:
-            return True
-        else:
-            return False
+        """
+        Checking status of homework
+        :return: bool
+        """
+        return (self.deadline + self.created) > self.created
 
 
 class Student:
+    """
+    Class which store information about students
+    """
     def __init__(self, last_name_string: str, first_name_string: str):
         self.last_name = last_name_string
         self.first_name = first_name_string
 
     def do_homework(self, homework: Homework):
+        """
+        Save solution of homework
+        :param homework: class Homework
+        :return: object HomeworkResult or error
+        """
         if homework.is_active():
             return homework
         else:
@@ -64,9 +76,18 @@ class Student:
 
 
 class Teacher:
+    """
+    Class which store information about teacher
+    """
     def __init__(self, last_name_string: str, first_name_string: str):
         self.last_name = last_name_string
         self.first_name = first_name_string
 
     def create_homework(self, text_of_task: str, day_number: int):
+        """
+        Function which create homework object
+        :param text_of_task: str
+        :param day_number: int
+        :return: object Homework
+        """
         return Homework(text_of_task, day_number)
